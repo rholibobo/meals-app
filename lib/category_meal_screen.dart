b8a17ed35dc9e00b13070a6ed951b1304b0548d5
rholibobo <rholibobo@gmail.com>
2023-10-12 19:29:46 +0100

    Displaying recipees...

--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealScreen extends StatelessWidget {
   static const routeName = '/category-meals';
   // final String categoryId;
@@ -15,14 +17,19 @@ class CategoryMealScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
       ),
-      body: Center(
-          child: Text(
-        'The Recipes for the Category',
-      )),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }
