e4eed46a08b09219fd07a7ee3c3fec0d09e0afab
rholibobo <rholibobo@gmail.com>
2023-10-12 15:53:23 +0100

    creating app folder structure

---
+++ lib/categories_screen.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: <Widget>[
+        
+      ],
+    );
+  }
+}
